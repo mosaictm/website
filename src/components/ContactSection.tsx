30799716bcf6755950cbdd0299d20305be0bf0a3
NightwalkerNPNG <shakapaka235@gmail.com>
2025-05-17 22:14:47 +0300

    a

--- src/components/ContactSection.tsx
+++ src/components/ContactSection.tsx
@@ -308,7 +308,7 @@ const ContactSection = () => {
             </div>
             
             {/* Social Media */}
-            <div className="mr-[-3rem] lg:mr-[-2rem]">
+            <div className="">
               <h3 className="text-xl font-bold mb-4">تابعنا على</h3>
               <div className="flex space-x-4 space-x-reverse">
                 <a 
